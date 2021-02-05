8d26539e1b858ee5ea6e822d95021a7437310e05
trismegistus-root <edward.baldwin@ucdenver.edu>
2021-02-05 00:08:13 -0700

    partitioner deploys and inherits replatform

--- contracts/replatform.sol
+++ contracts/replatform.sol
@@ -5,13 +5,18 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import './replatformPartitioner.sol';
 
 contract replatform is ERC20Capped, Ownable{
-    uint256 MAX_SUPPLY = 10000;
+    uint256 public MAX_SUPPLY;
+    uint256 public TOTAL_TREASURY;
+    string public _name;
+    string public _symbol;
 
-  constructor() 
+  constructor(MAX_SUPPLY, TOTAL_TREASURY, _name, _symbol) 
     ERC20Capped(MAX_SUPPLY) //Sets max token supply to 10,000 with 18 decimals
-    ERC20("Replatform", "NGMI")
+    ERC20(_name, _symbol)
     Ownable(){} //Sets name to Replatform, symbol to NGMI
 
-    //function to mint total supply to contract
     //function to funnel by 18th decimal into partitioner
+    function increaseTreasury(uint256 amount) public onlyOwner(){
+      TOTAL_TREASURY += TOTAL_TREASURY + amount;
+    }
 }
--- contracts/replatformPartitioner.sol
+++ contracts/replatformPartitioner.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+
 import '@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol';
+import './replatform.sol';
 
-contract replatformPartitioner{
+contract replatformPartitioner is ERC1155PresetMinterPauser, replatform{
+  constructor() public
+  ERC1155PresetMinterPauser(.json)
+  replatform(10000 ether, 20000000000, "Replatform", "NGMI"){}
+  
 
+
+  
 }
