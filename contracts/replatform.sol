cc4ac5ba102a33604a4711da361617e49735f9fd
trismegistus-root <edward.baldwin@ucdenver.edu>
2021-02-04 23:20:30 -0700

    setting basic understandings with ERC20 funnel contract

--- contracts/replatform.sol
+++ contracts/replatform.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
-import '@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './replatformPartitioner.sol';
 
-contract replatform is ERC1155, ERC1155PresetMinterPauser{
+contract replatform is ERC20Capped, Ownable{
+    uint256 MAX_SUPPLY = 10000;
 
+  constructor() 
+    ERC20Capped(MAX_SUPPLY) //Sets max token supply to 10,000 with 18 decimals
+    ERC20("Replatform", "NGMI"){} //Sets name to Replatform, symbol to NGMI
+
+    //function to mint total supply to contract
+    //function to funnel by 18th decimal into partitioner
 }
---
+++ contracts/replatformPartitioner.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol';
+
+contract replatformPartitioner{
+
+}
