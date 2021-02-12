99070bd0c414090dbee6cfe5f250b1562e680c97
trismegistus-root <edward.baldwin@ucdenver.edu>
2021-02-11 20:15:13 -0700

    joinCommunity function added

--- contracts/Replatform.sol
+++ contracts/Replatform.sol
@@ -14,15 +14,24 @@ contract Replatform is ERC20Treasury, ERC1155{
   ERC20Treasury(nameOfTreasury, symbolOfTreasury, cappedSupplyOfTreasury, initialAvailableSupplyOfTreasury)
   ERC1155("./../URI/URI.json") public {}
 
+    mapping(uint256 => uint256) internal communityId;
+    mapping(uint256 => address) internal communityOwner;
 
     function batch(address to, uint256 id, uint256 amount, bytes memory data) public batchOwner(){
       require(amount <= totalTreasury(), "Cannot withdraw more to batch than is available in treasury");
-      require(amount <= 1, "1 per wallet");
       require(_partition[to] == 0);
       withdrawFromTreasuryToBatch(amount);
       ERC1155._mint(to, id, amount, data);
+      communityId[id] = amount; //sets amount to community fund
+      communityOwner[id] = to; //sets address of owner
     }
-    
-  
+
+    function joinCommunity(uint256 id){
+      require(communityId[id] > 0, "Community has reached capacity");
+      safeTransferFrom(communityOwner[id], msg.sender, 1, 1);
+    }
+     
+
+
   
 }
